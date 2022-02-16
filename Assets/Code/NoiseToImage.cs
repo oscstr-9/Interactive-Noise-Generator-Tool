fbe8759ce2f9eebfeef28c9408f848344a7f8688
Oscar Östryd <oscstr-9@student.ltu.se>
2022-02-16 18:22:53 +0100

    Different version of Perlin noise implemented and working

--- Assets/Code/NoiseToImage.cs
+++ Assets/Code/NoiseToImage.cs
@@ -15,44 +15,68 @@ public class NoiseToImage : MonoBehaviour
 
     public void UploadPNG(int noiseNum)
     {
-        Perlin pNoise = gameObject.GetComponent<Perlin>();
-        BlueNoise bNoise = gameObject.GetComponent<BlueNoise>();
-
         // Create a texture the size of the screen, RGB24 format
-        int width = Screen.width;
-        int height = Screen.height;
+        int width = 128;
+        int height = 128;
         Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
         GetComponent<Renderer>().material.mainTexture = tex;
 
-        // Read screen contents into the texture
-        for (int y = 0; y < height; y++){
-            for (int x = 0; x < width; x++){
-                Color color = Color.green;
-                switch (noiseNum){
-                case 1:
-                // White Noise
-                    color = ((Random.Range(1, 3)) <=1 ? Color.black : Color.white);
-                    break;
-                case 2:
-                // Blue Noise
-                color = Color.blue;
-                    break;
-                case 3:
-                // Perlin Noise
-                    double colorValue = pNoise.OctavePerlin(x/width,y/height, 1, 8, 256);
-                    color = new Color((float)colorValue,0.0f,0.5f,1.0f);
-                    break;
-                case 4:
-                // Worley Noise
-                    color = Color.red;
-                    break;
-                default:
-                    break;
+        Perlin pNoise = gameObject.GetComponent<Perlin>();
+        pNoise.PerlinNoise(width, height);
+        BlueNoise bNoise = gameObject.GetComponent<BlueNoise>();
+
+        Color color = Color.green;
+        switch (noiseNum){
+        case 1:
+        // White Noise
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    color = ((Random.Range(1, 3)) <= 1 ? Color.black : Color.white);
+                    tex.SetPixel(x, y, color);
                 }
-                
-                tex.SetPixel(x, y, color);
             }
+            break;
+        case 2:
+        // Blue Noise
+            color = Color.blue;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    tex.SetPixel(x, y, color);
+                }
+            }            
+
+            break;
+        case 3:
+         // Perlin Noise
+            List<float> perlinNoise = pNoise.GetfPerlinNoise2D();
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    color = new Color((float)perlinNoise[y * width + x], (float)perlinNoise[y * width + x], (float)perlinNoise[y * width + x], 1.0f);
+                    tex.SetPixel(x, y, color);
+                }
+            }
+            break;
+        case 4:
+        // Worley Noise
+            color = Color.red;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    tex.SetPixel(x, y, color);
+                }
+            }
+            break;
+        default:
+            break;
         }
+
         tex.Apply();
 
         // Encode texture into PNG
@@ -61,18 +85,5 @@ public class NoiseToImage : MonoBehaviour
 
         // For testing purposes, also write to a file in the project folder
         File.WriteAllBytes(Application.dataPath + "/../Assets/TestPNGs/SavedScreen.png", bytes);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Space)){
-            print(i);
-            i+=1;
-            if(i>4){
-                i=1;
-            }
-            UploadPNG(i);
-        }
     }
 }
